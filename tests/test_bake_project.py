bbc2bc451edcdc79a4f9d89988f1bac1c15563e0
Paul Harrison <paul@smoothml.com>
2020-02-06 08:22:04 +0000

    Test project created correctly.

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -1,12 +1,42 @@
+import os
 from contextlib import contextmanager
 from cookiecutter.utils import rmtree
 
 
 @contextmanager
-def bake_in_temp_dif(cookiesm, *args, **kwargs):
+def inside_dir(dirpath):
+    """
+    Execute code from inside the given directory
+    :param dirpath: String, path of the directory the command is being run.
+    """
+    old_path = os.getcwd()
+
+    try:
+        os.chdir(dirpath)
+        yield
+    finally:
+        os.chdir(old_path)
+
+
+@contextmanager
+def bake_in_temp_dir(cookies, *args, **kwargs):
+    """
+    Delete the temporal directory that is created when executing the tests
+    :param cookies: pytest_cookies.Cookies,
+        cookie to be baked and its temporal files will be removed
+    """
     result = cookies.bake(*args, **kwargs)
 
     try:
         yield result
     finally:
         rmtree(str(result.project))
+
+
+
+def test_bake_with_defaults(cookies):
+    with bake_in_temp_dir(cookies) as result:
+        assert result.project.isdir()
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert result.project.basename == 'project-name'
