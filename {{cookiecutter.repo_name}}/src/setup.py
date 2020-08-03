21052d293ce624114b1a3a80c88b094b84b82b28
Paul Harrison <paulharrison@fastmail.com>
2020-08-03 14:05:04 +0100

    Feature/update code style (#12)
    * Format with Black.
    
    * Change lint to format.
    
    * Add Black to requirements.

--- {{cookiecutter.repo_name}}/src/setup.py
+++ {{cookiecutter.repo_name}}/src/setup.py
@@ -2,18 +2,18 @@ import re
 from pathlib import Path
 from setuptools import find_packages, setup
 
-NAME = '{{ cookiecutter.repo_name }}'
-DESCRIPTION = '{{ cookiecutter.description }}'
-REQUIRES_PYTHON = '>=3.5.0'
-AUTHOR = '{{ cookiecutter.author_name }}'
+NAME = "{{ cookiecutter.repo_name }}"
+DESCRIPTION = "{{ cookiecutter.description }}"
+REQUIRES_PYTHON = ">=3.5.0"
+AUTHOR = "{{ cookiecutter.author_name }}"
 
 ROOT_DIR = Path(__file__).resolve().parent
-PACKAGE_DIR = ROOT_DIR / '{{ cookiecutter.package_name }}'
+PACKAGE_DIR = ROOT_DIR / "{{ cookiecutter.package_name }}"
 
 # Get package version
-VERSION = '0.0.1' # Default version
+VERSION = "0.0.1"  # Default version
 version_pattern = re.compile("(?<=__version__\s=\s['\"])\d\.\d\.?\d?")
-with open(PACKAGE_DIR / '__init__.py') as f:
+with open(PACKAGE_DIR / "__init__.py") as f:
     for line in f:
         res = version_pattern.search(line.strip())
         if res is not None:
@@ -23,10 +23,10 @@ REQUIREMENTS = []
 
 setup(
     name=NAME,
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=["tests"]),
     version=VERSION,
     description=DESCRIPTION,
     python_requires=REQUIRES_PYTHON,
     install_requires=REQUIREMENTS,
-    author=AUTHOR
+    author=AUTHOR,
 )
